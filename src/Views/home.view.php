8fc47385ef8a4e2d2a0741f78db4304700ca20bf
DfrtJulien <de.freitas.julien78@gmail.com>
2024-10-05 12:52:02 +0200

    fin de la nav et footer

--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -1,1 +1,9 @@
+<?php
+require_once(__DIR__ . '/partials/head.php');
+?>
+
 <h1>Hello</h1>
+
+<?php
+include_once(__DIR__ . '/partials/footer.php');
+?>
--- src/Views/partials/footer.php
+++ src/Views/partials/footer.php
@@ -1,9 +1,4 @@
-</div>
-<footer class="text-center text-light myFooter">
-  <div class="text-center p-3">
-    © Copyright : Julien <?php echo date("Y") ?>
-  </div>
-</footer>
+<footer class="container-fluid bg-color-1 text-white fs-3"><?php echo date('Y') ?> © Julien De Freitas</footer>
 </body>
 
 </html>
